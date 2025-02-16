4644eedda2425fe1682a756ab1ab66b8d73beb16
Thomas Greening <tomfgreening@gmail.com>
2025-02-16 14:10:23 +0000

    Writing code to display all current posts in the databse on this page.

--- week9assignment/src/app/posts/page.js
+++ week9assignment/src/app/posts/page.js
@@ -1,5 +1,12 @@
 import {db} from "@/app/utilities/dbconnection";
 
-export default async function PostsPage (
-    
-)
+export default async function PostsPage() {
+    const posts = await db.query ('SELECT * FROM posts');
+    const wrangledPosts = posts.rows;
+    return (
+        <>
+        <h1> Pingr Posts Page </h1>
+        {wrangledPosts.map}
+        </>
+    );
+}
