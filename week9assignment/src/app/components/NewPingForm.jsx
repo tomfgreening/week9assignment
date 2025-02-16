f9d8ca18e738e772344c075932e64868821d76e6
Thomas Greening <tomfgreening@gmail.com>
2025-02-16 17:29:51 +0000

    Building new ping form component.

--- week9assignment/src/app/components/NewPingForm.jsx
+++ week9assignment/src/app/components/NewPingForm.jsx
@@ -1,2 +1,13 @@
 import { db } from "@/app/utilities/dbconnection";
-export default function NewPingForm() {}
+export default function NewPingForm() {
+    "use server";
+    async function handleSubmit (formValues) {
+        const post_title = formValues.get ("post_title");
+        const post_date = formValues.get ("post_date");
+        const post_body = formValues.get ("post_body");
+        db.query(
+            'INSERT INTO users (post_title, post_date, post_body) VALUES ($1, $2, $3)',
+            [post_title, post_date, post_body]
+        );
+    }
+}
