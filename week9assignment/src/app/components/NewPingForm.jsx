fc7f92816987e63be3dcf9f942faddac07db6253
Thomas Greening <tomfgreening@gmail.com>
2025-02-16 18:29:57 +0000

    .

--- week9assignment/src/app/components/NewPingForm.jsx
+++ week9assignment/src/app/components/NewPingForm.jsx
@@ -1,17 +1,17 @@
 import { db } from "@/app/utilities/dbconnection";
 export default function NewPingForm() {
-    "use server";
-    async function handleSubmit (formValues) {
-        const post_title = formValues.get ("post_title");
-        const post_date = formValues.get ("post_date");
-        const post_body = formValues.get ("post_body");
-        db.query(
-            'INSERT INTO users (post_title, post_date, post_body) VALUES ($1, $2, $3)',
-            [post_title, post_date, post_body]
-        );
-    }
-}
-return (
+  "use server";
+  async function handleSubmit(formValues) {
+    const post_title = formValues.get("post_title");
+    const post_date = formValues.get("post_date");
+    const post_body = formValues.get("post_body");
+    db.query(
+      "INSERT INTO users (post_title, post_date, post_body) VALUES ($1, $2, $3)",
+      [post_title, post_date, post_body]
+    );
+  }
+
+  return (
     <>
       <h1>Ping something!</h1>
       <form action={handleSubmit}>
@@ -25,5 +25,4 @@ return (
       </form>
     </>
   );
-
-
+}
